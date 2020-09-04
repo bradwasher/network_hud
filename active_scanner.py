e71ddc18778b72c331d5e038a9b8e59adadfa220
bradwasher <13262221+bradwasher@users.noreply.github.com>
2020-09-04 17:30:51 -0400

    Add files via upload
    Added nmap scans.

--- active_scanner.py
+++ active_scanner.py
@@ -19,6 +19,47 @@ class ActiveScanner:
             stderr=subprocess.DEVNULL
         )
 
+    @staticmethod
+    def nmap_1000(ip):
+        # nmap --top-ports 1000 ip
+        subprocess.Popen(
+            ['nmap', '--top-ports', '1000', ip],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+
+    @staticmethod
+    def nmap_100(ip):
+        # nmap --top-ports 100 ip
+        subprocess.Popen(
+            ['nmap', '--top-ports', '100', ip],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+
+    @staticmethod
+    def nmap_topports_udp(ip):
+        # nmap -sTU --top-ports
+        subprocess.Popen(
+            ['nmap', '-sU', '--top-ports', '1000', ip],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+
+    @staticmethod
+    def nmap_mikrotik(ip):
+        # nmap -nn -p80,8080,443,8291,8292,9281
+        # nmap -sTU --top-ports
+        subprocess.Popen(
+            ['nmap', '-sTU', '-p80,8080,443,8291,8292,9281', ip],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+
+    @staticmethod
+    def nmap_OS(ip):
+        pass
+
 
 def main():
     ActiveScanner.arp_scan_local()
